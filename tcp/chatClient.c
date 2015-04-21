1ac316c745d1f5aca803214d264288c9411196bf
Tony Thomas <01tonythomas@gmail.com>
2015-04-21 19:38:10 +0530

    optimized code, removed junk

--- tcp/chatClient.c
+++ tcp/chatClient.c
@@ -9,42 +9,35 @@
 #include <errno.h>
 #include <ctype.h>
 #include <netdb.h>
-
 int main() {
 	struct sockaddr_in server, client;
 	unsigned int sockfd,serverlen;
+	char msg[100], recvmsg[100];
 
-	if ( ( sockfd = socket( PF_INET, SOCK_STREAM, 0 ) ) < 0 ) {
+	if ( ( sockfd = socket( AF_INET, SOCK_STREAM, 0 ) ) < 0 ) {
 		perror( "socket" );
 	}
 	
 	bzero ((char*)&server, sizeof (server));
-	server.sin_family = PF_INET;
+	server.sin_family = AF_INET;
 	inet_aton("127.0.0.1", &server.sin_addr); 
-	server.sin_port = htons (8699);
-	
-	bzero((char*) &client, sizeof(client));
-	client.sin_family = AF_INET;
-	inet_aton ("127.0.0.1", &client.sin_addr);
-	client.sin_port = htons (0);
+	server.sin_port = htons(8950);
 	
 	if ( connect( sockfd,(struct sockaddr*)&server, sizeof(server) ) < 0  ) {
 		perror( "Cannot connect to TCP" );
 	}
-	char msg[100], recvmsg[100];
-	bind (sockfd, (struct sockaddr*)&client, sizeof (client));
 	
 	for(;;) {
-
 		printf( "Client>> " );
 		scanf( " %[^\n]", msg );
 		if ( send( sockfd, msg, 100, 0  ) < 0 ) {
 			perror( "client sending send" );
 		}
+		
 		if ( recv( sockfd, recvmsg, 100, 0 ) < 0 ) {
 			perror( "client recieving failed" );
 		}
-		printf("Server says: %s\n", recvmsg );
+		printf("Server says >> %s \n", recvmsg );
 	}
 	close( sockfd );
 	
--- tcp/chatServer.c
+++ tcp/chatServer.c
@@ -11,19 +11,19 @@
 #include <sys/time.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-
-
 int main() {
 	struct sockaddr_in server, client;
 	unsigned int sockfd, clientlen, clientfd;
 	char rmsg[100], serverMessage[100];
 	
-	/*---- Create the socket. The three arguments are: ----*/
-	sockfd = socket (AF_INET, SOCK_STREAM, 0);
+	if ( ( sockfd = socket (AF_INET, SOCK_STREAM, 0 ) ) < 0 ) {
+		perror( "socket" );
+	}
+	
 	bzero ((char*) &server, sizeof (server)); 
 	server.sin_family = AF_INET;
 	inet_aton ("127.0.0.1", &server.sin_addr); 
-	server.sin_port = htons(8699);
+	server.sin_port = htons(8950);
 	
 	if ( bind ( sockfd, (struct sockaddr*)&server, sizeof (server) ) < 0 ) {
 		perror( "bind error " );
@@ -31,10 +31,12 @@ int main() {
 	if ( listen( sockfd, SOMAXCONN ) < 0 ) {
 		perror( "listen" );
 	}
+	
 	clientlen = sizeof( client );
 	clientfd = accept( sockfd, ( struct sockaddr * )&client, &clientlen );
+	
 	if ( clientfd <  0 ) {
-	  perror( "accept" );
+		perror( "accept" );
 	}
 	
 	for (;;) {
@@ -47,7 +49,6 @@ int main() {
 		if ( send( clientfd, serverMessage, 100, 0  ) < 0 ) { 
 			perror( "serever sending fail" );
 		}
-		printf("sent");
 	}
 	
 	close(clientfd);
