c7a0b82af949cabfe231379dfb1b28aa9665a87f
Tony Thomas <01tonythomas@gmail.com>
2017-02-04 16:22:32 +0100

    Add in the run script for the python implementation

--- chat-socket-python.py
+++ chat-socket-python.py
@@ -3,11 +3,33 @@ import socket, sys, threading
 # Copied from https://gist.githubusercontent.com/owainlewis/3217710/raw/2bda03005314ab32961f59b5439b9ce95547bace/socket_chat.py
 # Simple chat client that allows multiple connections via threads
 
-PORT = 9876 # the port number to run our server on
+PORT = 1337 # the port number to run our server on
 
 __version__ = "0.0.1"
 
 class ChatServer(threading.Thread):
+    
+    storeddata = ""
+    
+    helpmessage = """!help: A cubby hole is a small hiding place where one can hide things.
+!help: We now define the cubby hole protocol that allows users to store
+!help: one line messages on a server. 
+!help: As the hole is really small, the server will only store one
+!help: message at a time, but keeps and shares it across different
+!help: connections. If a new message is put in the cubby hole, the
+!help: old message is lost.
+!help:
+!help: The following commands should be supported:
+!help:
+!help: PUT <msg> Places a new message in the cubby hole. 
+!help: GET       Takes the message out of the cubby hole and displays it.
+!help: LOOK      Displays message without taking it out of the cubby hole.
+!help: DROP      Takes the message out of the cubby hole without displaying it.
+!help: HELP      Displays some help message.
+!help: QUIT      Terminates the connection.
+!help:
+!help: Have Fun and play around!\n""" 
+
     
     def __init__(self, port, host='localhost'):
         threading.Thread.__init__(self)
@@ -30,32 +52,29 @@ class ChatServer(threading.Thread):
 
     def run_thread(self, conn, addr):
         print('Client connected with ' + addr[0] + ':' + str(addr[1]))
-        storeddata = ""
+        conn.sendall("!hello: welcome aboard\n")
         while True:
             data = conn.recv(1024).lower()
             if data.startswith('put '):
-                print("PUT")
                 datatobestored = data.split(" ")[1]
-                storeddata = datatobestored
-                conn.sendall("!PUT: %s"%storeddata)
+                self.storeddata = datatobestored
+                conn.sendall("!PUT: %s"%self.storeddata)
             elif data.startswith('get'):
-                print("GET")
-                if storeddata:
-                    conn.sendall("!GET: %s"%storeddata)
-                    storeddata= ""
+                if self.storeddata:
+                    conn.sendall("!GET: %s"%self.storeddata)
+                    self.storeddata= ""
             elif data.startswith('look'):
-                print("LOOK")
-                if storeddata:
-                    conn.sendall("!LOOK: %s"%storeddata)
+                if self.storeddata:
+                    conn.sendall("!LOOK: %s"%self.storeddata)
             elif data.startswith('drop '):
-                print("DROP")
-                if storeddata:
-                    storeddata= ""
-            elif data.startswith('help '):
-                print("HELP")
+                if self.storeddata:
+                    self.storeddata= ""
+            elif data.startswith('help'):
+                conn.sendall(self.helpmessage)
             elif data.startswith('quit '):
-                print("QUIT")
                 break
+            elif data: 
+                conn.sendall("ERROR: Invalid command. Please type help for HELP\n")
             elif not data: 
                 break
             
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python chat-socket-python.py
